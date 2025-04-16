766e41e54dc16365b121411d53c739e752e6b0ac
boltovignat <uncelpeter@gmail.com>
2025-04-16 19:05:39 +0300

    Отчет

--- local/templates/.default/components/bitrix/crm.company.list/.default/filterStatic/index.php
+++ local/templates/.default/components/bitrix/crm.company.list/.default/filterStatic/index.php
@@ -4,8 +4,9 @@ use Bitrix\Main\Page\Asset;
 $assetManager = \Bitrix\Main\Page\Asset::getInstance();
 $userId = $USER->getId();
 $userName = trim($USER->GetFirstName() . ' ' . $USER->GetLastName());
+$userGroups = CUser::GetUserGroup($userId);
 
-if (!$USER->IsAdmin()) {
+if (!$USER->IsAdmin() && !in_array(51, $userGroups)) {
 
 // Для подключения css
     Asset::getInstance()->addCss("/local/templates/.default/components/bitrix/crm.company.list/.default/style.css");
@@ -17,14 +18,14 @@ if (!$USER->IsAdmin()) {
             let isApplying = false;
             
             BX.addCustomEvent("Grid::ready", function(gridData) {
-                console.log("ready");
+                //console.log("ready");
                                
                 const filter = BX.Main.filterManager.getById(gridData.containerId);
                 if (filter) safeApplyFilter(filter);
             });
            
             BX.addCustomEvent("Grid::beforeRequest", function(gridData, args) {
-                console.log("beforeRequest");                
+                //console.log("beforeRequest");                
                 const filter = BX.Main.filterManager.getById(args.gridId);
                 if (filter) safeApplyFilter(filter);
             });
@@ -34,21 +35,23 @@ if (!$USER->IsAdmin()) {
             });
 
             function safeApplyFilter(filter) {
-                console.log("Изменение фильтра");
+                //console.log("Изменение фильтра");
                                 
                 if (isApplying) return false;
+                if (filter.presets.getPreset(filter.presets.getCurrentPresetId()).TITLE == `Свободные компании`) return false;
                 
                 try {
                     isApplying = true;
                     const values = filter.getFilterFieldsValues();
                     let needUpdate = false;
-                    
-                    if (!values["ASSIGNED_BY_ID"]?.includes(userId)) {
+                    debugger;
+                    //filter.presets.getPreset(filter.presets.getCurrentPresetId())
+                    if (!values["ASSIGNED_BY_ID"].includes(userId)) {
                         values["ASSIGNED_BY_ID"] = [userId];
                         needUpdate = true;
                     }
                     
-                    if (!values["ASSIGNED_BY_ID_label"]?.includes(userName)) {
+                    if (!values["ASSIGNED_BY_ID_label"].includes(userName)) {
                         values["ASSIGNED_BY_ID_label"] = [userName];
                         needUpdate = true;
                     }
@@ -58,7 +61,7 @@ if (!$USER->IsAdmin()) {
                         filter.getApi().apply();
                     }
                     
-                    console.log("Завершен");
+                    //console.log("Завершен");
 
                     return needUpdate;
                 } catch (e) {
@@ -71,7 +74,7 @@ if (!$USER->IsAdmin()) {
             
             // Просмотр событий всех
             /*BX.onCustomEvent = function(eventObject, eventName, arEventParams, secureParams) {
-                console.log(eventName);
+                //console.log(eventName);
             };*/
             
         })();
